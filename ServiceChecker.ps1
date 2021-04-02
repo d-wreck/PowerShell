f15f0c5f5bc626e35e00c23ccad2a90e6080d526
d-wreck <derekdep6@gmail.com>
2021-04-02 10:23:07 -1000

    Add files via upload

---
+++ ServiceChecker.ps1
@@ -0,0 +1,21 @@
+﻿# Basic script to check for the existence of a service and re-create it 
+
+$servicename = "TestService"
+
+If (Get-Service $servicename -ErrorAction SilentlyContinue){
+    # Service Exists already, no action needed
+}
+Else{
+    # Service got deleted, re-create it    
+    $params = @{
+      Name = $servicename
+      BinaryPathName = '"C:\Path\To\My\Binaries.exe"'
+      DisplayName = "Test Service"
+      StartupType = "Automatic"
+      Description = "Some Service Description"
+    }
+
+    # Create a new service with the splatted params and start it
+    New-Service @params
+    Start-Service $servcicename
+}
