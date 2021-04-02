f7a2c9e8e3dd3e0558812ba345843b2b1454eaa1
d-wreck <derekdep6@gmail.com>
2021-04-02 10:47:05 -1000

    Add files via upload

---
+++ CCDCPREP/ClearEventLogs.ps1
@@ -0,0 +1,5 @@
+﻿# Clear the specified event logs 
+Clear-EventLog -LogName 'Windows PowerShell'
+Clear-EventLog -LogName 'System'
+(New-Object System.Diagnostics.Eventing.Reader.EventLogSession).ClearLog("Microsoft-Windows-PowerShell/Operational")
+(New-Object System.Diagnostics.Eventing.Reader.EventLogSession).ClearLog("Microsoft-Windows-Sysmon/Operational")
